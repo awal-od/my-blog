7d3a16a3b713aaaf34a263546ad649c87d1096cf
awal-od <55546881+awal-od@users.noreply.github.com>
2019-10-28 13:52:06 -0400

    initial commit

---
+++ src/build/precache-manifest.1de1e9172332e5e70d6e07e9780bd1f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eee5534aa2734faa95c401d2ae9218c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d2d1243db083d86a5a8",
+    "url": "/static/css/main.a96b10f4.chunk.css"
+  },
+  {
+    "revision": "4862d0a1c78018b32abb",
+    "url": "/static/js/2.656bced6.chunk.js"
+  },
+  {
+    "revision": "5d2d1243db083d86a5a8",
+    "url": "/static/js/main.7afeb15f.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
